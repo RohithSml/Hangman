17a6c97e6b295613d3d3338125a45a290fa9f6f9
Rohith Samuel <rohith.sml94@gmail.com>
2019-02-11 05:23:26 +0530

    GREEN: Wrote Code for main function

--- hangman.py
+++ hangman.py
@@ -25,14 +25,15 @@ def mask_secret_word(word):
     return masked_wrd
 
 def check(word,userIp):
-    n=word.count(userIp)
+    slice_wrd=word[:]
+    n= slice_wrd.count(userIp)
     index_list=[]
-
     for i in range (n):
-        index=word.index(userIp)
+        index=slice_wrd.index(userIp)
         index_list.append(index)
-        word[index]='!'
-
+        slice_wrd[index]='@'
+        
+        
     return  index_list
     
 
@@ -41,3 +42,59 @@ def unmask(word,g_wrd,index_list):
         g_wrd[i]=word[i]
 
     return g_wrd
+
+
+
+def main(str_word=get_secret_word()):
+    tries=10
+    word=list(str_word)
+    
+    init_msk_str=mask_secret_word(str_word)
+    mskd_list=list(init_msk_str)
+    
+    wrng_g=''
+    g_str=''.join(mskd_list)
+    
+   # print(word)
+    
+    while tries:
+        
+        print(f"\n {g_str} \n")
+        print(f"Number of tries left: {tries}")
+        print(f"Wrong guesses so far: {wrng_g}")
+        
+        usr_input=input("Enter your guess:  ")
+        
+        if len(usr_input)!=1:
+            print("Enter one Char. only!")
+            continue
+        
+        if usr_input in word:
+            
+            index_list=check(word,usr_input)
+            mskd_list=unmask(word,mskd_list,index_list)
+            g_str=''.join(mskd_list)
+            
+            print(g_str)
+            
+            
+        else:
+            
+            print("Wrong Guess:")
+            wrng_g=wrng_g+usr_input
+            
+            
+            tries=tries-1
+            
+        if str_word==g_str:
+            
+            print("Congratulations! You WON.")
+            break
+            
+        if tries == 0:
+            
+            print(f"Too bad! The secret word was {str_word}")
+                
+                
+if __name__=='__main__':
+    main()
